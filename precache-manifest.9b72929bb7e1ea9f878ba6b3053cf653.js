73d27a0f77c3caa0ecb40bc4039057a7b185b3e0
Gustavo Primolan <gustavo.primolan@hotmail.com>
2021-04-05 11:52:38 -0300

    Updates

---
+++ precache-manifest.9b72929bb7e1ea9f878ba6b3053cf653.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b99785f3ee88b3299040f8e8b5479b35",
+    "url": "/sigo-frontend/index.html"
+  },
+  {
+    "revision": "48f7490674dcca56f60c",
+    "url": "/sigo-frontend/static/css/main.bb3cb2ac.chunk.css"
+  },
+  {
+    "revision": "5ae9a84cfa157e69290d",
+    "url": "/sigo-frontend/static/js/2.16420857.chunk.js"
+  },
+  {
+    "revision": "b34e485bffe2681834363c3faa902788",
+    "url": "/sigo-frontend/static/js/2.16420857.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48f7490674dcca56f60c",
+    "url": "/sigo-frontend/static/js/main.9712cdee.chunk.js"
+  },
+  {
+    "revision": "74475ba8fd65ab08b2ef",
+    "url": "/sigo-frontend/static/js/runtime-main.bdd67d9d.js"
+  },
+  {
+    "revision": "38975343b586296e9b73e6b56cc3ec5d",
+    "url": "/sigo-frontend/static/media/fa-brands-400.38975343.svg"
+  },
+  {
+    "revision": "8e49b728413079dfd9ee45d0c58d54e4",
+    "url": "/sigo-frontend/static/media/fa-brands-400.8e49b728.eot"
+  },
+  {
+    "revision": "9f018d1025561e96439f7c0e9026301a",
+    "url": "/sigo-frontend/static/media/fa-brands-400.9f018d10.woff"
+  },
+  {
+    "revision": "9f4ce3dc689981a1b87faab0f5484f9e",
+    "url": "/sigo-frontend/static/media/fa-brands-400.9f4ce3dc.woff2"
+  },
+  {
+    "revision": "b7d071b9c3c197bff4af902070622423",
+    "url": "/sigo-frontend/static/media/fa-brands-400.b7d071b9.ttf"
+  },
+  {
+    "revision": "7980a6361c25b4665dbbe92d4488783c",
+    "url": "/sigo-frontend/static/media/fa-regular-400.7980a636.woff2"
+  },
+  {
+    "revision": "7aaf5675efd6339e9aba53ecbe5f1e36",
+    "url": "/sigo-frontend/static/media/fa-regular-400.7aaf5675.woff"
+  },
+  {
+    "revision": "859001f6ae8eb0bb3878aaa971b50fc6",
+    "url": "/sigo-frontend/static/media/fa-regular-400.859001f6.eot"
+  },
+  {
+    "revision": "da8a235bb207c74eea21507f3a86a53b",
+    "url": "/sigo-frontend/static/media/fa-regular-400.da8a235b.svg"
+  },
+  {
+    "revision": "f33342516f7cbe46f1d6b68f9e7bbeda",
+    "url": "/sigo-frontend/static/media/fa-regular-400.f3334251.ttf"
+  },
+  {
+    "revision": "0be94a07755ba9b88f2ebcac0f23a3da",
+    "url": "/sigo-frontend/static/media/fa-solid-900.0be94a07.woff"
+  },
+  {
+    "revision": "64b3e814a66c2719b15abf8f7998bd73",
+    "url": "/sigo-frontend/static/media/fa-solid-900.64b3e814.woff2"
+  },
+  {
+    "revision": "7726a281c1d436eb038f78c6e9048c96",
+    "url": "/sigo-frontend/static/media/fa-solid-900.7726a281.svg"
+  },
+  {
+    "revision": "e2675a616b68f446fa6284c111554c7f",
+    "url": "/sigo-frontend/static/media/fa-solid-900.e2675a61.eot"
+  },
+  {
+    "revision": "f14c3b2ff7c821a4c838debbffd6ad2d",
+    "url": "/sigo-frontend/static/media/fa-solid-900.f14c3b2f.ttf"
+  }
+]);
